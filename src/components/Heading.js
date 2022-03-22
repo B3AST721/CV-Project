78b15b77193bce4af4e7ed6fff1979aa25312eba
Jaheel <jaheelwarr@gmail.com>
2022-03-21 21:53:56 -0600

    added tailwind and heading

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Heading extends Component {
+  render() {
+    return (
+      <div>
+          <h1 className='text-4xl font-bold flex justify-center align-center'>CV Project</h1>
+      </div>
+    )
+  }
+}
+
+export default Heading
